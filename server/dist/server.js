aa0d5e2e78dc5e01224f0b3456d19f8efaf0ec4d
Acero Liang <aceroliang@Aceros-MacBook-Air.local>
2024-01-23 19:58:36 -0500

    fixed mongo connection

---
+++ server/dist/server.js
@@ -0,0 +1,44 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongodb_1 = require("mongodb");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const uri = "mongodb+srv://Cluster92290:dawg123123123@cluster92290.vr1l9yv.mongodb.net/?retryWrites=true&w=majority";
+const app = (0, express_1.default)();
+const port = 3000;
+app.use((0, cors_1.default)());
+const client = new mongodb_1.MongoClient(uri, {
+    serverApi: {
+        version: mongodb_1.ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield client.connect();
+            yield client.db("admin").command({ ping: 1 });
+            console.log("successfully connected to mongodb");
+            app.listen(port, () => {
+                console.log(`server listening on port ${port}`);
+            });
+        }
+        catch (_a) {
+            yield client.close();
+        }
+    });
+}
+run().catch(console.dir);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,33 @@
-import express, { Express, Request, Response, Application } from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import express from 'express';
 import cors from 'cors';
 
-const app: Application = express();
-const port: number = 8000;
+const uri = "mongodb+srv://Cluster92290:dawg123123123@cluster92290.vr1l9yv.mongodb.net/?retryWrites=true&w=majority";
+const app = express();
+const port = 3000;
 app.use(cors());
-app.listen(port, () => {
-    console.log("listening on port 8000")
-})
 
-app.get('/wow', (req, res) => {
-    res.send("connected to server");
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("successfully connected to mongodb");
+        app.listen(port, () => {   
+            console.log(`server listening on port ${port}`);
+        })
+    } catch {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
