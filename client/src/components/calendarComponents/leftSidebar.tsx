bad326248165cfb2399e3c1de9bcb67691f4f6c0
Acero Liang <aceroliang@Aceros-MacBook-Air.local>
2024-01-23 19:20:42 -0500

    added notes

--- client/src/components/calendarComponents/leftSidebar.tsx
+++ client/src/components/calendarComponents/leftSidebar.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import MiniCalendar from './MiniCalendar'; 
 import '../calendarCSS/LeftSidebar.css';
 
@@ -9,7 +9,23 @@ interface LeftSidebarProps {
     setViewDate: Function;
     today: Date;
 }
+
+/** The left side bar also has notes, it handles everything within the left sidebar */
 const LeftSidebar: FC<LeftSidebarProps> = ({showModal, setShowModal, viewDate, setViewDate, today }) => {
+    const [note, setNote] = useState('');
+    var notes: { [date: string ]: string } = {}; // NOTE: the key, date is of string type but is actually a Date object's toDateString() output 
+    
+    function handleNoteChange(str: string) {
+        // we are going to set notes variable
+        // store the note in the dictionary, or change it 
+        notes[viewDate.toDateString()] = str;
+        console.log(notes);
+    }
+
+    /** getNote: given a date, returns the note as a string. Returns the empty string if there is no note */
+    function getNote(date: Date): string {
+        return notes[date.toDateString()];
+    }
     return (
         <div className="left-sidebar">
             {/* Add event sets modal state to (true) (show), which shows the modal*/}
@@ -20,6 +36,9 @@ const LeftSidebar: FC<LeftSidebarProps> = ({showModal, setShowModal, viewDate, s
                 viewDate={viewDate}
                 setViewDate={setViewDate}
             />
+            <div className="add-event-description-container">
+                <textarea placeholder="Add a note" className="add-event-description" value={getNote(viewDate)} onChange={(e) => handleNoteChange(e.target.value)}></textarea>
+            </div>
         </div>
     )
 }
