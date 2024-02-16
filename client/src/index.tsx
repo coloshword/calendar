d99eeebdf25752fc95ecbe563b8829a98ea2172a
Acero Liang <aceliang2001@gmail.com>
2024-02-16 01:16:25 -0500

    afd

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import { createRoot } from "react-dom/client";
+import  ReactDOM  from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import { AuthProvider } from './components/AuthProvider'
 
-const rootElement = document.getElementById("root") as HTMLElement;
-const root = createRoot(rootElement)
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
-root.render(
+// const rootElement = document.getElementById("root") as HTMLElement;
+// const root = createRoot(rootElement)
+
+ReactDOM.render(
     <React.StrictMode>
       <AuthProvider>
         <App />
       </AuthProvider>
     </React.StrictMode>,
-);
+document.getElementById('root'));
 
 reportWebVitals();
 
