38337c62c586f32c12f02c2fa1109fa63590c4fc
Acero Liang <aceliang2001@gmail.com>
2024-02-16 01:04:39 -0500

    fdadfs

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import { AuthProvider } from './components/AuthProvider'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.getElementById("root") as HTMLElement;
+const root = createRoot(rootElement)
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
 root.render(
     <React.StrictMode>
       <AuthProvider>
